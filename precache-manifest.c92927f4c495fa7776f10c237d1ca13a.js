f3b178e659129c73d14c94f21505d68c4af7e93b
Binh Lam <cindybinh444@gmail.com>
2019-01-07 18:31:54 -0500

    Updates

---
+++ precache-manifest.c92927f4c495fa7776f10c237d1ca13a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e110b738f01ad5de67bd",
+    "url": "/v3_personal_portfolio/static/css/main.b84efcff.chunk.css"
+  },
+  {
+    "revision": "e110b738f01ad5de67bd",
+    "url": "/v3_personal_portfolio/static/js/main.e110b738.chunk.js"
+  },
+  {
+    "revision": "43a35c0b738977b6d40a",
+    "url": "/v3_personal_portfolio/static/js/1.43a35c0b.chunk.js"
+  },
+  {
+    "revision": "31a9f54627c64658be50",
+    "url": "/v3_personal_portfolio/static/js/runtime~main.31a9f546.js"
+  },
+  {
+    "revision": "0e18d4f223cb674a3a820fccc4627ea3",
+    "url": "/v3_personal_portfolio/static/media/bcphoto.0e18d4f2.jpg"
+  },
+  {
+    "revision": "bba59c24fc365a5995d46cc5d278edc0",
+    "url": "/v3_personal_portfolio/static/media/blurPhoto.bba59c24.jpg"
+  },
+  {
+    "revision": "96bd34ff28652500593bfe876454c848",
+    "url": "/v3_personal_portfolio/static/media/moreBlurPhoto.96bd34ff.jpg"
+  },
+  {
+    "revision": "219c4d693b7c4c3c727f6f4935b9d32e",
+    "url": "/v3_personal_portfolio/static/media/begslist_app.219c4d69.png"
+  },
+  {
+    "revision": "74468e9f2eee53aa1f8150e17b139cba",
+    "url": "/v3_personal_portfolio/static/media/recipe_app.74468e9f.jpg"
+  },
+  {
+    "revision": "cfeef3d6dc348d0e15da63323c9896ef",
+    "url": "/v3_personal_portfolio/static/media/news_app.cfeef3d6.jpg"
+  },
+  {
+    "revision": "4a195dd859cd31c3af97b557f4cbcb8e",
+    "url": "/v3_personal_portfolio/static/media/click_app.4a195dd8.jpeg"
+  },
+  {
+    "revision": "c73c0fc12c11ab477b74286dd54b6e4b",
+    "url": "/v3_personal_portfolio/static/media/trivia_app.c73c0fc1.jpg"
+  },
+  {
+    "revision": "5aa47605f13b95692d82a7df61bd4c6d",
+    "url": "/v3_personal_portfolio/static/media/liri_app.5aa47605.jpg"
+  },
+  {
+    "revision": "5410f47bcccf6845aa70b89d6781b21d",
+    "url": "/v3_personal_portfolio/static/media/rain.5410f47b.jpg"
+  },
+  {
+    "revision": "3f84ae19914e6a0eab0b70dacd5c83f8",
+    "url": "/v3_personal_portfolio/index.html"
+  }
+];
