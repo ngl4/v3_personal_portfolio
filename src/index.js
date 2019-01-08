96fd5b8a7faf44d174f3a861c2fee04f3b5d104e
Binh Lam <cindybinh444@gmail.com>
2019-01-08 12:57:26 -0500

    reset back to original

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
