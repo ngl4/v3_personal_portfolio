523bef26445ea7e5d3aecc6d78800d579ed8104c
Binh Lam <cindybinh444@gmail.com>
2019-02-09 23:40:52 -0500

    add wave underline effect for sideNav

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -2,14 +2,13 @@ import React, { Component } from "react";
 import "./SideNav.css";
 
 class SideNav extends Component {
-
   openNav = () => {
     this.refs.mySidenav.style.width = "60%";
-  }
-  
+  };
+
   closeNav = () => {
     this.refs.mySidenav.style.width = "0";
-  }
+  };
   render() {
     return (
       <nav className="fixed-top nav-color">
@@ -17,16 +16,23 @@ class SideNav extends Component {
           <div className="closebtn" onClick={this.closeNav}>
             &times;
           </div>
-          <a href={this.props.AboutUrl}>About</a>
-          <a href={this.props.PortfolioUrl}>Portfolio</a>
-          <a href={this.props.ContactUrl}>Contact</a>
+          <ul className="waves">
+            <li>
+              <a href={this.props.AboutUrl}>About</a>
+            </li>
+            <li>
+              <a href={this.props.PortfolioUrl}>Portfolio</a>
+            </li>
+            <li>
+              <a href={this.props.ContactUrl}>Contact</a>
+            </li>
+          </ul>
         </div>
 
         <span className="nav-icon" onClick={this.openNav}>
           {/* &#9776; */}
           <img src={this.props.image} alt="logo" className="img-logo" />
         </span>
-
       </nav>
     );
   }
